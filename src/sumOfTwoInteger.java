a9b5640a8b0768399ce01ea4888b7ca70152c384
Ellen Liu <allenliu789@gmail.com>
2018-10-05 14:49:06 -0700

    Adding all files

---
+++ src/sumOfTwoInteger.java
@@ -0,0 +1,13 @@
+
+public class sumOfTwoInteger {
+	public static int addTwoInt(int int1, int int2) {
+		return int1 + int2;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(addTwoInt(1,2));
+		
+	}
+	
+}
